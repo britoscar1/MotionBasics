bf57154854b38b8e4b67990d1756a1d02a67f954
britoscar1 <brito.oscar@uabc.edu.nx>
2026-06-16 17:35:54 -0700

    Initial Commit

---
+++ MotionBasics/MotionBasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotionBasicsApp.swift
+//  MotionBasics
+//
+//  Created by Oscar Artemio Brito Ortiz on 16/06/26.
+//
+
+import SwiftUI
+
+@main
+struct MotionBasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
